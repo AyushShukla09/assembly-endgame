a85b21ae9befb2ae038ece63107d631ec1b1cec6
AyushShukla09 <hbkayushshukla09@gmail.com>
2025-04-05 23:30:23 +0530

    Intial commit for assembly endgame

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Hangmanh(){
+    return (
+        <header>
+            <h1>Assembly: Endgame</h1>
+            <p>Guess the work in under 8 attempts to keep the programming world safe from Assembly</p>
+            <section className="game-status">
+                <h2>You Win</h2>
+                <p>Well Done!</p>
+            </section>
+        </header>
+    )
+}
