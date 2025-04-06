dc85154be1f7d75943a7da313bcb82edeff19a7a
AyushShukla09 <hbkayushshukla09@gmail.com>
2025-04-06 15:30:43 +0530

    Added languages and blank box

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,46 @@
-import React from "react"
+import React from "react";
+import { languages } from "./languages";
 
-export default function Hangmanh(){
+export default function Hangman() {
+
+    const [currentWord, setCurrentWord] = React.useState("React")
+
+  const languageElements = languages.map((ele) => {
+    const style = {
+      background: ele.backgroundColor,
+      color: ele.color,
+    };
     return (
-        <header>
-            <h1>Assembly: Endgame</h1>
-            <p>Guess the work in under 8 attempts to keep the programming world safe from Assembly</p>
-            <section className="game-status">
-                <h2>You Win</h2>
-                <p>Well Done!</p>
-            </section>
-        </header>
+      <span 
+        className="chips" 
+        style={style}
+        key={ele.name}>
+        {ele.name}
+      </span>
+    );
+  });
+  const letters = currentWord.split('').map((ele,index)=>{
+    return (
+        <span key={index}>{ele.toUpperCase()}</span>
     )
+  })
+  return (
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>
+          Guess the work in under 8 attempts to keep the programming world safe
+          from Assembly
+        </p>
+      </header>
+      <section className="game-status">
+        <h2>You Win</h2>
+        <p>Well Done!</p>
+      </section>
+      <section className="language-chips">{languageElements}</section>
+      <section className="word">
+        {letters}
+      </section>
+    </main>
+  );
 }
