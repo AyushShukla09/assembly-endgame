f0869e3686362af8558be6e2a32db37d4c79a8b2
AyushShukla09 <hbkayushshukla09@gmail.com>
2025-04-09 00:46:33 +0530

    Added Guessed letters

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,14 @@ import React from "react";
 import { languages } from "./languages";
 
 export default function Hangman() {
-
-    const [currentWord, setCurrentWord] = React.useState("React")
+  const [currentWord, setCurrentWord] = React.useState("REACT");
+  const [guessedLetter, setGuessedLetter] = React.useState([]);
+  console.log(guessedLetter);
+  function handleGuessedLetter(newLetter) {
+    setGuessedLetter((prev) => {
+      return !prev.includes(newLetter) ? [...prev, newLetter] : [...prev];
+    });
+  }
 
   const languageElements = languages.map((ele) => {
     const style = {
@@ -11,25 +17,22 @@ export default function Hangman() {
       color: ele.color,
     };
     return (
-      <span 
-        className="chips" 
-        style={style}
-        key={ele.name}>
+      <span className="chips" style={style} key={ele.name}>
         {ele.name}
       </span>
     );
   });
-  const letters = currentWord.split('').map((ele,index)=>{
+  const letters = currentWord.split("").map((ele, index) => {
+    return <span key={index}>{ele.toUpperCase()}</span>;
+  });
+  const alphabets = "abcdefghijklmnopqrstuvwxyz";
+  const keyboard = alphabets.split("").map((val) => {
     return (
-        <span key={index}>{ele.toUpperCase()}</span>
-    )
-  })
-  const alphabets = 'abcdefghijklmnopqurstuvwxyz'
-  const keyboard = alphabets.split('').map((val,index)=>{
-    return (
-      <button key={index}>{val.toUpperCase()}</button>
-    )
-  })
+      <button key={val} onClick={() => handleGuessedLetter(val)}>
+        {val.toUpperCase()}
+      </button>
+    );
+  });
   return (
     <main>
       <header>
@@ -44,12 +47,8 @@ export default function Hangman() {
         <p>Well Done!</p>
       </section>
       <section className="language-chips">{languageElements}</section>
-      <section className="word">
-        {letters}
-      </section>
-      <section className="keyboard">
-        {keyboard}
-      </section>
+      <section className="word">{letters}</section>
+      <section className="keyboard">{keyboard}</section>
       <button className="new-game">New Game</button>
     </main>
   );
